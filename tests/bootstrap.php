367a7bee4d104a00a58bf60f854eba0e461ff577
jubianchi <contact@jubianchi.fr>
2013-01-21 22:12:52 +0100

    Improve atoum bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,6 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../vendor/atoum/atoum/scripts/runner.php';
+
+if (class_exists('\\mageekguy\\atoum\\autoloader') === false) {
+    require_once __DIR__ . '/../vendor/atoum/atoum/scripts/runner.php';
+}
