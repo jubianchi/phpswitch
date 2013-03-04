5bcd1c5853e4e60405699ce54dbe22dc7aa27408
jubianchi <contact@jubianchi.fr>
2013-03-05 00:27:44 +0100

    Add stub

---
+++ src/jubianchi/PhpSwitch/Phar/Stub.php
@@ -0,0 +1,25 @@
+<?php
+namespace jubianchi\PhpSwitch\Phar;
+
+class Stub
+{
+    public function __toString()
+	{
+		return <<<'EOF'
+#!/usr/bin/env php
+<?php
+
+$PHAR = basename(__FILE__);
+$ROOT = 'phar://' . $PHAR . DIRECTORY_SEPARATOR;
+$BIN = $ROOT . 'bin' . DIRECTORY_SEPARATOR . basename($PHAR, '.phar');
+
+Phar::mapPhar($PHAR);
+
+if(file_exists($BIN)) {
+	require $BIN;
+}
+
+__HALT_COMPILER();
+EOF;
+	}
+}
