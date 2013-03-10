ddf5e469384c32fb0cb40220e9bd344bc15f9e11
jubianchi <contact@jubianchi.fr>
2013-03-10 23:36:08 +0100

    Use installer in php:install command

--- src/jubianchi/PhpSwitch/Console/Command/PHP/InstallCommand.php
+++ src/jubianchi/PhpSwitch/Console/Command/PHP/InstallCommand.php
@@ -15,15 +15,6 @@ class InstallCommand extends Command
     const NAME = 'php:install';
     const DESC = 'Installs a PHP version';
 
-    /** @var string */
-    private $archive;
-
-    /** @var string */
-    private $source;
-
-    /** @var string */
-    private $prefix;
-
     /** @var \jubianchi\PhpSwitch\PHP\Option\Option[] */
     private $options = array();
 
@@ -50,6 +41,7 @@ class InstallCommand extends Command
     {
         parent::setApplication($application);
 
+
         if (null !== $application) {
             foreach ($application->getOptionFinder() as $option) {
                 $option = new $option();
@@ -59,14 +51,88 @@ class InstallCommand extends Command
         }
     }
 
+    protected function getSubscriber(OutputInterface $output)
+    {
+        $self = $this;
+        $subscriber = new \jubianchi\PhpSwitch\Event\Subscriber();
+        $indent = self::INDENT;
+
+        $afterCallback = function() use($output) { $output->write(PHP_EOL); };
+        $processCallback = function($event) use ($self, $output) {
+            $self->log($event['buffer'], 'err' === $event['type'] ? \Monolog\Logger::ERROR : \Monolog\Logger::INFO);
+            $self->getHelper('progress')->advance();
+        };
+
+        $subscriber
+            ->handle('install.before', function($event) use($output) {
+                $output->writeln(
+                    array(
+                        sprintf('Installing PHP <info>%s</info>', $event['version']->getVersion()),
+                        sprintf('From mirror <info>%s</info>', $event['mirror']),
+                        sprintf('Configure options: <info>[%s]</info>', $event['options'])
+                    )
+                );
+            })
+            ->handle('install.after', function($event) use($output, $indent) {
+                $output->writeln(array(
+                    sprintf(PHP_EOL . 'PHP version <info>%s</info> was installed:', $event['version']),
+                    sprintf('%s<comment>%s</comment>', $indent, $event['prefix'])
+                ));
+            })
+            ->handle('download.before', function($event) use($self, $output, $indent) {
+                $output->writeln(array(
+                    sprintf(PHP_EOL . 'Downloading PHP <info>%s</info>', $event['version']->getVersion()),
+                    sprintf('%s<comment>%s</comment>', $indent, sprintf($event['version']->getUrl(), $event['mirror']))
+                ));
+
+                if (OutputInterface::VERBOSITY_VERBOSE !== $output->getVerbosity()) {
+                    $self->startProgress($output, 100, '[%bar%] %percent%%');
+                }
+            })
+            ->handle('download.progress', function($size, $downloaded) use ($self) {
+                static $previous = 0;
+
+                if ($size > 0) {
+                    $complete = ceil(($downloaded / $size) * 100);
+
+                    $self->getHelper('progress')->advance($complete - $previous);
+
+                    $previous = $complete;
+                }
+            })
+            ->handle('download.after', $afterCallback)
+            ->handle('extract.before', function($event) use($self, $output, $indent) {
+                $output->writeln(array(
+                    sprintf(PHP_EOL . 'Extracting <info>%s</info>', $event['version']->getVersion()),
+                    sprintf('%s<comment>%s</comment>', $indent, $event['archive'])
+                ));
+
+                $self->startProgress($output);
+            })
+            ->handle('extract.progress', $processCallback)
+            ->handle('extract.after', $afterCallback)
+            ->handle('build.before', function($event) use($self, $output, $indent) {
+                $output->writeln(array(
+                    sprintf(PHP_EOL . 'Building <info>%s</info>', $event['version']->getVersion()),
+                    sprintf('%s<comment>%s</comment>', $indent, $event['prefix'])
+                ));
+
+                $self->startProgress($output);
+            })
+            ->handle('build.progress', $processCallback)
+            ->handle('build.after', $afterCallback)
+        ;
+
+        return $subscriber;
+    }
+
     /**
      * @param \Symfony\Component\Console\Input\InputInterface   $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
      *
-     * @throws \RuntimeException
      * @throws \InvalidArgumentException
      *
-     * @return int
+     * @return int|void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
@@ -74,61 +140,17 @@ class InstallCommand extends Command
 
         $finder = $this->getApplication()->getService('app.php.finder');
         $version = $finder->getVersion($input->getArgument('version'));
-
+        $mirror = $this->getConfiguration()->get('mirror');
         if (null !== ($alias = $input->getOption('alias'))) {
             $version->setName($alias);
         }
+        $options = $this->resolveOptions($input);
 
-        $dest = $this->getBuilder()->getDestination($version);
-        if (true === file_exists($dest)) {
-            throw new \RuntimeException(sprintf('PHP version %s is already installed', $version));
-        }
-
-        $options = $this->getResolver()->resolve($input, $this->options);
-
-        if (null !== ($config = $input->getOption('config'))) {
-            try {
-                $config = $this->getConfiguration()->get('versions.' . str_replace('.', '-', $config));
-            } catch (\InvalidArgumentException $exception) {
-                throw new \InvalidArgumentException(
-                    sprintf('Configuration %s does not exist', $config),
-                    $exception->getCode(),
-                    $exception
-                );
-            }
-
-            $options = array_merge(
-                $this->getNormalizer()->denormalize($config, $this->options),
-                $options
-            );
-        }
-
-        $options = array_unique($options);
-        $normalized = $this->getNormalizer()->normalize($options);
-
-        $output->writeln(
-            array(
-                sprintf('Installing PHP <info>%s</info>', $version->getVersion()),
-                sprintf('Configure options: <info>[%s]</info>', $normalized)
-            )
-        );
-
-        foreach ($options as $option) {
-            $option->preInstall($version, $input, $output);
-        }
-
-        $this
-            ->download($version, $output)
-            ->extract($version, $output)
-            ->install($version, $this->getNormalizer()->normalize($options), $input->getOption('jobs'), $output)
-            ->getConfiguration()
-                ->set('versions.' . str_replace('.', '-', $version), $normalized)
-                ->dump()
+        $this->getApplication()->getService('app.event.dispatcher')->addEventSubscriber($this->getSubscriber($output));
+        $this->getInstaller()
+            ->setOptions($options)
+            ->install($version, $mirror, $input->getOption('jobs'), $input, $output)
         ;
-
-        foreach ($options as $option) {
-            $option->postInstall($version, $input, $output);
-        }
 
         foreach ($input->getOption('ini') as $ini) {
             if (false !== ($ini = parse_ini_string($ini))) {
@@ -136,14 +158,25 @@ class InstallCommand extends Command
             }
         }
 
-        $output->writeln(array(
-            sprintf(PHP_EOL . 'PHP version <info>%s</info> was installed:', $version->getVersion()),
-            sprintf('%s<comment>%s</comment>', self::INDENT, $this->prefix)
-        ));
+        $this->getConfiguration()
+            ->set('versions.' . str_replace('.', '-', $version), $options->normalize())
+            ->dump()
+        ;
 
         return 0;
     }
 
+    /**
+     * @return \jubianchi\PhpSwitch\PHP\Installer
+     */
+    public function getInstaller()
+    {
+        return $this->getApplication()->getService('app.php.installer');
+    }
+
+    /**
+     * @return \jubianchi\PhpSwitch\PHP\Option\Resolver
+     */
     public function getResolver()
     {
         return $this->getApplication()->getOptionResolver();
@@ -157,7 +190,28 @@ class InstallCommand extends Command
         return $this->getApplication()->getOptionNormalizer();
     }
 
-    protected function startProgress(OutputInterface $output, $max = null, $format = '[%bar%]')
+    protected function resolveOptions(InputInterface $input)
+    {
+        $options = $this->getResolver()->resolve($input, $this->options);
+
+        if (null !== ($config = $input->getOption('config'))) {
+            try {
+                $config = $this->getConfiguration()->get('versions.' . str_replace('.', '-', $config));
+            } catch (\InvalidArgumentException $exception) {
+                throw new \InvalidArgumentException(
+                    sprintf('Configuration %s does not exist', $config),
+                    $exception->getCode(),
+                    $exception
+                );
+            }
+
+            $options->addOptions($this->getNormalizer()->denormalize($config, $this->options));
+        }
+
+        return $options;
+    }
+
+    public function startProgress(OutputInterface $output, $max = null, $format = '[%bar%]')
     {
         $progress = $this->getHelper('progress');
 
@@ -167,165 +221,5 @@ class InstallCommand extends Command
         $progress->setFormat($format);
 
         $progress->start($output, $max);
-    }
-
-    /**
-     * @return \jubianchi\PhpSwitch\PHP\Downloader
-     */
-    public function getDownloader()
-    {
-        return $this->getApplication()->getDownloader();
-    }
-
-    /**
-     * @param \jubianchi\PhpSwitch\PHP\Version                  $version
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     *
-     * @return \jubianchi\PhpSwitch\Console\Command\PHP\InstallCommand
-     */
-    protected function download(Version $version, OutputInterface $output)
-    {
-        $dest = $this->getDownloader()->getDestination($version);
-        if (false === file_exists($dest)) {
-            $mirror = $this->getConfiguration()->get('mirror');
-
-            $output->writeln(array(
-                sprintf(PHP_EOL . 'Downloading PHP <info>%s</info>', $version->getVersion()),
-                sprintf('%s<comment>%s</comment>', self::INDENT, sprintf($version->getUrl(), $mirror))
-            ));
-
-            $this->getDownloader()->download($version, $mirror, $this->getDownloadProgressCallback($output));
-
-            $output->writeln('');
-        }
-
-        $this->archive = $dest;
-
-        return $this;
-    }
-
-    /**
-     * @return \jubianchi\PhpSwitch\PHP\Extracter
-     */
-    public function getExtracter()
-    {
-        return $this->getApplication()->getExtracter();
-    }
-
-    /**
-     * @param \jubianchi\PhpSwitch\PHP\Version                  $version
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     *
-     * @return \jubianchi\PhpSwitch\Console\Command\PHP\InstallCommand
-     */
-    protected function extract(Version $version, OutputInterface $output)
-    {
-        $dest = $this->getExtracter()->getDestination($version);
-        if (false === file_exists($dest)) {
-            $output->writeln(array(
-                sprintf(PHP_EOL . 'Extracting <info>%s</info>', $version->getVersion()),
-                sprintf('%s<comment>%s</comment>', self::INDENT, $dest)
-            ));
-
-            $this->getExtracter()->extract($version, $this->archive, $this->getProcessCallback($output));
-
-            $output->writeln('');
-        }
-
-        $this->source = $dest;
-
-        return $this;
-    }
-
-    /**
-     * @return \jubianchi\PhpSwitch\PHP\Builder
-     */
-    public function getBuilder()
-    {
-        return $this->getApplication()->getBuilder();
-    }
-
-    /**
-     * @param \jubianchi\PhpSwitch\PHP\Version                  $version
-     * @param string                                            $options
-     * @param int                                               $jobs
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     *
-     * @throws \RuntimeException
-     *
-     * @return \jubianchi\PhpSwitch\Console\Command\PHP\InstallCommand
-     */
-    protected function install(Version $version, $options, $jobs, OutputInterface $output)
-    {
-        $dest = $this->getBuilder()->getDestination($version);
-        $output->writeln(array(
-            sprintf(PHP_EOL . 'Building <info>%s</info>', $version->getVersion()),
-            sprintf('%s<comment>%s</comment>', self::INDENT, $dest)
-        ));
-
-        $this->getBuilder()->build($version, $this->source, $options, $jobs, $this->getProcessCallback($output));
-        mkdir($dest . '/var/db', 0777, true);
-
-        $ini = $this->source . DIRECTORY_SEPARATOR . 'php.ini-development';
-        $destination = $dest . DIRECTORY_SEPARATOR . 'etc' . DIRECTORY_SEPARATOR . 'php.ini';
-
-        $output->writeln(array(
-            PHP_EOL . PHP_EOL . 'Moving configuration file',
-            sprintf('%s<comment>From: %s</comment>', self::INDENT, $ini),
-            sprintf('%s<comment>To: %s</comment>', self::INDENT, $destination)
-        ));
-
-        if (false === is_dir(dirname($destination))) {
-            mkdir(dirname($destination), 0755, true);
-        }
-
-        copy($ini, $destination);
-
-        $this->prefix = $dest;
-
-        return $this;
-    }
-
-    /**
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     *
-     * @return callable
-     */
-    protected function getProcessCallback(OutputInterface $output)
-    {
-        $self = $this;
-
-        if (OutputInterface::VERBOSITY_VERBOSE !== $output->getVerbosity()) {
-            $this->startProgress($output);
-        }
-
-        return function($type, $buffer) use ($self, $output) {
-            $buffer = rtrim($buffer);
-            if ('' === empty($buffer)) {
-                return;
-            }
-
-            $self->log($buffer, 'err' === $type ? \Monolog\Logger::ERROR : \Monolog\Logger::INFO);
-            $self->getHelper('progress')->advance();
-        };
-    }
-
-    protected function getDownloadProgressCallback(OutputInterface $output)
-    {
-        $self = $this;
-
-        $this->startProgress($output, 100, '[%bar%] %percent%%');
-
-        return function($download_size, $downloaded_size, $upload_size, $uploaded_size) use ($self) {
-            static $previous = 0;
-
-            if ($download_size > 0) {
-                $complete = ceil(($downloaded_size / $download_size) * 100);
-
-                $self->getHelper('progress')->advance($complete - $previous);
-
-                $previous = $complete;
-            }
-        };
     }
 }
