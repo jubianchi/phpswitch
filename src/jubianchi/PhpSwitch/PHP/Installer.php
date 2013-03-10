16b49024610b2d366be946fbcf4da2f4990745df
jubianchi <contact@jubianchi.fr>
2013-03-10 23:35:24 +0100

    Add installer

---
+++ src/jubianchi/PhpSwitch/PHP/Installer.php
@@ -0,0 +1,156 @@
+<?php
+namespace jubianchi\PhpSwitch\PHP;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use jubianchi\PhpSwitch\PHP\Option\OptionCollection;
+use jubianchi\PhpSwitch\Event\Emitter;
+use jubianchi\PhpSwitch\Event\Dispatcher;
+
+class Installer extends Emitter
+{
+    /** @var \jubianchi\PhpSwitch\PHP\Builder */
+    private $builder;
+
+    /** @var \jubianchi\PhpSwitch\PHP\Downloader */
+    private $downloader;
+
+    /** @var \jubianchi\PhpSwitch\PHP\Extracter */
+    private $extracter;
+
+    /** @var \jubianchi\PhpSwitch\PHP\Option\OptionCollection */
+    protected $options;
+
+    /**
+     * @param \jubianchi\PhpSwitch\PHP\Downloader   $downloader
+     * @param \jubianchi\PhpSwitch\PHP\Extracter    $extracter
+     * @param \jubianchi\PhpSwitch\PHP\Builder      $builder
+     * @param \jubianchi\PhpSwitch\Event\Dispatcher $dispatcher
+     */
+    public function __construct(Downloader $downloader, Extracter $extracter, Builder $builder, Dispatcher $dispatcher = null)
+    {
+        $this->downloader = $downloader;
+        $this->extracter = $extracter;
+        $this->builder = $builder;
+
+        if (null !== $dispatcher) {
+            $this->setDispatcher($dispatcher);
+        }
+    }
+
+    /**
+     * @param \jubianchi\PhpSwitch\PHP\Option\OptionCollection $options
+     *
+     * @return \jubianchi\PhpSwitch\PHP\Installer
+     */
+    public function setOptions(OptionCollection $options)
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+
+    public function getNormalizedOptions()
+    {
+        return (string) $this->options;
+    }
+
+    public function install(Version $version, $mirror, $jobs, InputInterface $input, OutputInterface $output)
+    {
+        $dest = $this->builder->getDestination($version);
+        $this->emit(
+            'install.before',
+            $args = array(
+                'version' => $version,
+                'mirror' => $mirror,
+                'jobs' => $jobs,
+                'options' => $this->options,
+                'prefix' => $dest
+            )
+        );
+
+        if (is_dir($dest)) {
+            throw new \RuntimeException(sprintf('PHP version %s is already installed', $version));
+        }
+
+        if (null !== $this->options) {
+            $this->options->preInstall($version, $input, $output);
+        }
+
+        $archive = $this->download($version, $mirror);
+        $source = $this->extract($version, $archive);
+        $this->make($version, $source, $this->getNormalizedOptions(), $jobs);
+
+        if (null !== $this->options) {
+            $this->options->postInstall($version, $input, $output);
+        }
+
+        $this->emit('install.after', $args);
+
+        return $this;
+    }
+
+    /**
+     * @param \jubianchi\PhpSwitch\PHP\Version $version
+     * @param string                           $mirror
+     *
+     * @return \jubianchi\PhpSwitch\PHP\Installer
+     */
+    protected function download(Version $version, $mirror)
+    {
+        $archive = $this->downloader->getDestination($version);
+
+        if (false === file_exists($archive)) {
+            $this->downloader->download($version, $mirror);
+        }
+
+        return $archive;
+    }
+
+    /**
+     * @param \jubianchi\PhpSwitch\PHP\Version $version
+     * @param string                           $archive
+     *
+     * @return \jubianchi\PhpSwitch\PHP\Installer
+     */
+    protected function extract(Version $version, $archive)
+    {
+        $source = $this->extracter->getDestination($version);
+
+        if (false === file_exists($source)) {
+            $this->extracter->extract($version, $archive);
+        }
+
+        return $source;
+    }
+
+    /**
+     * @param \jubianchi\PhpSwitch\PHP\Version $version
+     * @param string                           $source
+     * @param string                           $options
+     * @param int                              $jobs
+     *
+     * @throws \RuntimeException
+     *
+     * @return \jubianchi\PhpSwitch\PHP\Installer
+     */
+    protected function make(Version $version, $source, $options, $jobs)
+    {
+        $prefix = $this->builder->getDestination($version);
+        $this->builder->build($version, $source, $options, $jobs);
+        mkdir($prefix . '/var/db', 0755, true);
+
+        $ini = $source . DIRECTORY_SEPARATOR . 'php.ini-development';
+        $destination = $prefix . DIRECTORY_SEPARATOR . 'etc' . DIRECTORY_SEPARATOR . 'php.ini';
+
+        if (false === is_dir(dirname($destination))) {
+            mkdir(dirname($destination), 0755, true);
+        }
+
+        if (is_file($ini)) {
+            copy($ini, $destination);
+        }
+
+        return $prefix;
+    }
+}
---
+++ tests/units/jubianchi/PhpSwitch/PHP/Installer.php
@@ -0,0 +1,103 @@
+<?php
+namespace tests\units\jubianchi\PhpSwitch\PHP;
+
+use mageekguy\atoum;
+use mageekguy\atoum\mock\stream;
+use jubianchi\PhpSwitch\PHP\Installer as TestedClass;
+
+require_once __DIR__ . '/../../../bootstrap.php';
+
+class Installer extends atoum\test
+{
+    public function testClass()
+    {
+        $this
+            ->testedClass
+                ->isSubClassOf('\\jubianchi\\PhpSwitch\\Event\\Emitter')
+        ;
+    }
+
+    public function test__construct()
+    {
+        $this
+            ->if($downloader = new \jubianchi\PhpSwitch\PHP\Downloader(uniqid()))
+            ->and($extracter = new \jubianchi\PhpSwitch\PHP\Extracter(uniqid()))
+            ->and($builder = new \jubianchi\PhpSwitch\PHP\Builder(uniqid()))
+            ->then
+                ->object($installer = new TestedClass($downloader, $extracter, $builder))->isInstanceOf('\\jubianchi\\PhpSwitch\\PHP\\Installer')
+        ;
+    }
+
+    public function testSetOptions()
+    {
+        $this
+            ->if($downloader = new \jubianchi\PhpSwitch\PHP\Downloader(uniqid()))
+            ->and($extracter = new \jubianchi\PhpSwitch\PHP\Extracter(uniqid()))
+            ->and($builder = new \jubianchi\PhpSwitch\PHP\Builder(uniqid()))
+            ->and($options = new \mock\jubianchi\PhpSwitch\PHP\Option\OptionCollection(array()))
+            ->and($this->calling($options)->__toString = $normalized = uniqid())
+            ->and($installer = new TestedClass($downloader, $extracter, $builder))
+            ->then
+                ->object($installer->setOptions($options))->isIdenticalTo($installer)
+                ->string($installer->getNormalizedOptions())->isEqualTo($normalized)
+        ;
+    }
+
+    public function testInstall()
+    {
+        $this
+            ->if($root = stream::get())
+            ->and($downloader = new \mock\jubianchi\PhpSwitch\PHP\Downloader(stream::getSubStream($root)))
+            ->and($this->calling($downloader)->download = $downloader)
+            ->and($this->calling($downloader)->getDestination = $archive = stream::get())
+            ->and($extracter = new \mock\jubianchi\PhpSwitch\PHP\Extracter(stream::getSubStream($root)))
+            ->and($this->calling($extracter)->extract = $extracter)
+            ->and($this->calling($extracter)->getDestination = $source = stream::get())
+            ->and($builder = new \mock\jubianchi\PhpSwitch\PHP\Builder($installDir = stream::getSubStream($root, uniqid())))
+            ->and($this->calling($builder)->build = $builder)
+            ->and($dispatcher = new \mock\jubianchi\PhpSwitch\Event\Dispatcher())
+            ->and($options = new \mock\jubianchi\PhpSwitch\PHP\Option\OptionCollection(array()))
+            ->and($this->calling($options)->normalize = $normalized = uniqid())
+            ->and($version = new \jubianchi\PhpSwitch\PHP\Version(phpversion()))
+            ->and($input = new \mock\Symfony\Component\Console\Input\InputInterface())
+            ->and($output = new \mock\Symfony\Component\Console\Output\OutputInterface())
+            ->and($process = new \mock\Symfony\Component\Process\Process(uniqid()))
+            ->and($this->calling($process)->run = 0)
+            ->and($processBuilder = new \mock\Symfony\Component\Process\ProcessBuilder())
+            ->and($this->calling($processBuilder)->getProcess = $process)
+            ->and($installer = new \mock\jubianchi\PhpSwitch\PHP\Installer($downloader, $extracter, $builder, $dispatcher))
+            ->and($installer->setOptions($options))
+            ->then
+                ->object($installer->install($version, $mirror = uniqid(), null, $input, $output))
+                ->mock($downloader)
+                    ->call('download')->withArguments($version, $mirror)->once()
+                ->mock($extracter)
+                    ->call('extract')->withArguments($version, $archive)->once()
+                ->mock($builder)
+                    ->call('build')->withArguments($version, $source, $normalized, null)->once()
+                ->mock($options)
+                    ->call('preInstall')->withArguments($version, $input, $output)->once()
+                    ->call('postInstall')->withArguments($version, $input, $output)->once()
+                ->mock($installer)
+                    ->call('emit')->withArguments(
+                        'install.before',
+                        $args = array(
+                            'version' => $version,
+                            'mirror' => $mirror,
+                            'jobs' => null,
+                            'options' => $options,
+                            'prefix' => $builder->getDestination($version)
+                        )
+                    )->once()
+                    ->call('emit')->withArguments('install.after', $args)->once()
+                ->if($dir = stream::getSubStream($installDir, 'php-' . phpversion()))
+                ->and($dir->dir_opendir = true)
+                ->then
+                    ->exception(function() use($installer, $version, $input, $output) {
+                        $installer->install($version, uniqid(), null, $input, $output);
+                    })
+                        ->isInstanceOf('\\RuntimeException')
+                        ->hasMessage(sprintf('PHP version %s is already installed', $version))
+        ;
+    }
+}
