db41c1fb78d235302a6edea6f66cc2478307776c
Julien Bianchi <julien.bianchi@pmsipilot.com>
2013-04-02 12:17:57 +0200

    WIP

--- .atoum.treemap.php
+++ .atoum.treemap.php
@@ -50,4 +50,128 @@ $script
 			return (int) trim($commit);
 		}
 	))
+    ->addAnalyzer(new \mageekguy\atoum\scripts\treemap\analyzer\generic(
+        'decision',
+        'Decisions',
+        function(\splFileInfo $file)
+        {
+            $tokenFilter = function($token) {
+                if (is_array($token))
+                {
+                    switch ($token[0])
+                    {
+                        case T_IF:
+                        case T_ELSE:
+                        case T_ELSEIF:
+                        case T_FOR:
+                        case T_FOREACH:
+                        case T_WHILE:
+                        case T_CASE:
+                        case T_DEFAULT:
+                            return true;
+
+                        default:
+                            return false;
+                    }
+                }
+
+                return false;
+            };
+
+            return sizeof(array_filter(token_get_all(file_get_contents((string) $file)), $tokenFilter));
+        }
+    ))
+    ->addAnalyzer(new \mageekguy\atoum\scripts\treemap\analyzer\generic(
+        'methods',
+        'Methods',
+        function(\splFileInfo $file)
+        {
+            $methods = 0;
+            $current = null;
+            $tokens = token_get_all(file_get_contents((string) $file));
+
+            for ($i = 0; $i < sizeof($tokens); $i++)
+            {
+                $token = $tokens[$i];
+                $current = is_array($token) ? $token[0] : $token;
+
+                if(in_array($current, array(T_PUBLIC, T_PROTECTED, T_PRIVATE)))
+                {
+                    do {
+                        $next = $tokens[++$i];
+                        $next = is_array($next) ? $next[0] : $next;
+                    } while(in_array($next, array(' ', T_WHITESPACE)));
+
+                    if ($next === T_FUNCTION)
+                    {
+                        $methods++;
+                    }
+                }
+            }
+
+            return $methods;
+        }
+    ))
+    ->addAnalyzer(new \mageekguy\atoum\scripts\treemap\analyzer\generic(
+        'complexity',
+        'Complexity',
+        function(\splFileInfo $file)
+        {
+            $tokenFilter = function($token) {
+                if (is_array($token))
+                {
+                    switch ($token[0])
+                    {
+                        case T_IF:
+                        case T_ELSE:
+                        case T_ELSEIF:
+                        case T_FOR:
+                        case T_FOREACH:
+                        case T_WHILE:
+                        case T_CASE:
+                        case T_DEFAULT:
+                            return true;
+
+                        default:
+                            return false;
+                    }
+                }
+
+                return false;
+            };
+
+            $methodFilter = function(\splFileInfo $file)
+            {
+                $methods = 0;
+                $current = null;
+                $tokens = token_get_all(file_get_contents((string) $file));
+
+                for ($i = 0; $i < sizeof($tokens); $i++)
+                {
+                    $token = $tokens[$i];
+                    $current = is_array($token) ? $token[0] : $token;
+
+                    if(in_array($current, array(T_PUBLIC, T_PROTECTED, T_PRIVATE)))
+                    {
+                        do {
+                            $next = $tokens[++$i];
+                            $next = is_array($next) ? $next[0] : $next;
+                        } while(in_array($next, array(' ', T_WHITESPACE)));
+
+                        if ($next === T_FUNCTION)
+                        {
+                            $methods++;
+                        }
+                    }
+                }
+
+                return $methods;
+            };
+
+            $decisions = sizeof(array_filter(token_get_all(file_get_contents((string) $file)), $tokenFilter));
+            $methods = $methodFilter($file);
+
+            return ($decisions - $methods) < 0 ? 0 : ($decisions - $methods);
+        }
+    ))
 ;
